0db13177de660f142fd4e5baae7e1348add835cd
erickadje <erickadje96@gmail.com>
2019-06-27 18:44:57 +0100

    add of sanbox mode

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[WIP]
+# KKIAPAY PHP-SDK
 
 ## Raw Files
 
@@ -6,19 +6,68 @@
     git clone https://github.com/kkiapay/php-sdk.git
 ```
 
-## Using Composer
-Run the following command to install this package using composer dep management system :
+
+ 
+## Installing
+
+  
+
+Using composer:
+  
 
 ```bash
+
     composer require kkiapay/kkiapay-php
+
 ```
 
+  
 
-## Usage
+## Request to retrieve transactions 
 
-#### Verify transaction status using thier Id
+#### EXAMPLE
 ```php
-    $kkiapay = new \Kkiapay\Kkiapay($public_key, $private_key, $secret);
-
+    $kkiapay = new \Kkiapay\Kkiapay($public_key, $private_key, $secret, $sandbox=false);
     $kkiapay->verifyTransaction($transaction_id);
 ```
+
+## Request to revert transaction 
+
+#### EXAMPLE
+
+```php
+    $kkiapay = new \Kkiapay\Kkiapay($public_key, $private_key, $secret, $sandbox=false);
+    $kkiapay->refundTransaction($transaction_id);
+```
+
+## Request to schedule payout 
+
+#### EXAMPLE
+
+```php
+    $kkiapay = new \Kkiapay\Kkiapay($public_key, $private_key, $secret, $sandbox=false);
+    $kkiapay->refundTransaction($algorithm, $send_notification, 
+        $destination_type, $roof_amount, $destination,$rate_frequency);
+```
+
+| PARAMETERS      | DESCRIPTION             |
+| ----------- | ----------------------- |
+|  $algorithm    | (String)   "roof" or ""                 |
+| $send_nofitication      | (Boolean)                    |
+| $destination_type    | (String) "BANK_ACCOUNT" or "MOBILE_MONEY"              |
+| $roof_amount | (String)                     |
+| $destination |  (String)  |
+| $rate_frequency | (String) "1m", "1w", "3j"  |
+
+
+## COMPLETE  POSSIBLE STATUS LIST
+
+| STATUS      | DESCRIPTION             |
+| ----------- | ----------------------- |
+|  SUCCESS    |        Successful transaction                 |
+| FAILED      |         Transaction failed                |
+| INSUFFICIENT_FUND    | Not enough money in developper  account              |
+| TRANSACTION_NOT_ELIGIBLE | This transaction  are already reverted or are not eligible                    |
+| TRANSACTION_NOT_FOUND |  Transaction not found |
+| INVALID_TRANSACTION | You are not owner of this transaction  |
+| INVALID_TRANSACTION_TYPE | We can't revert this transaction  |
--- src/Kkiapay.php
+++ src/Kkiapay.php
@@ -1,5 +1,8 @@
 <?php namespace Kkiapay;
 
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
+use function GuzzleHttp\json_encode;
 /**
  * Created by PhpStorm.
  * User: shadai.ali
@@ -24,14 +27,17 @@ class Kkiapay{
 
     private $curl;
 
+    private $sandbox;
+
     /**
      * Kkiapay constructor.
      */
-    public function __construct($public_key, $private_key, $secret = null)
+    public function __construct($public_key, $private_key, $secret = null, $sandbox = false)
     {
         $this->private_key = $private_key;
         $this->public_key = $public_key;
         $this->secret = $secret;
+        $this->sandbox = $sandbox;
         $this->curl = new \GuzzleHttp\Client();
     }
 
@@ -44,21 +50,96 @@ class Kkiapay{
     public function verifyTransaction($transactionId){
         $response = null;
       try{
-          $response = $this->curl->post(Constants::BASE_URL. '/api/v1/transactions/status', array(
-              "json" => array("transactionId" => $transactionId),
-              'headers' => [
-                  'Accept'     => 'application/json',
-                  'X-API-KEY'      => $this->private_key
-              ]
-          ));
+        
+        $const = $this->sandbox ? Constants::SANDBOX_URL : Constants::BASE_URL;
 
-          $response = $response->getBody();
+        $response = $this->curl->post($const. '/api/v1/transactions/status', array(
+            "json" => array("transactionId" => $transactionId),
+            'headers' => [
+                'Accept' => 'application/json',
+                'X-API-KEY' => $this->public_key,
+                'X-PRIVATE-KEY' => $this->private_key,
+                'X-SECRET-KEY' => $this->secret
+            ]
+        ));
+
+        $response = $response->getBody();
       }catch (\Exception $e){
 
-         $response = json_encode(array( "status" => STATUS::TRANSACTION_NOT_FOUND));
+        $response = json_encode(array( "status" => STATUS::TRANSACTION_NOT_FOUND));
       }
     return json_decode((string)$response);
     }
+
+
+    public function refundTransaction($transactionId){
+        $reponse = null;
+      try{
+
+        $const = $this->sandbox ? Constants::SANDBOX_URL : Constants::BASE_URL;
+
+          $response = $this->curl->post($const. '/api/v1/transactions/revert', array(
+              "json" => array("transactionId" => $transactionId),
+              'headers' => [
+                  'Accept'     => 'application/json',
+                  'X-API-KEY'      => $this->public_key
+              ]
+          ));
+
+            $reponse = $response->getBody();
+            return json_decode((string)$reponse);
+
+        }catch (RequestException $e){
+            if ($e->hasResponse()) {
+                $reponse = "{".$this->get_string_between(Psr7\str($e->getResponse()), "{","}")."}";
+
+                return json_decode((string)$reponse);
+            }
+            $reponse = json_encode(array( "status" => STATUS::FAILED));
+            return json_decode((string)$response);
+        }
+    }
+
+    public function setupPayout($algorithm,$send_notification,$destination_type,$roof_amount,$destination,$rate_frequency){
+        $reponse = null;
+      try{
+
+          $const = $this->sandbox ? Constants::SANDBOX_URL : Constants::BASE_URL;
+
+          $response = $this->curl->post($const. '/api/v1/merchant/payouts/setup-payout', array(
+              "json" => array( "algorithm" => $algorithm, "send_notification" => $send_notification, "destination_type" => $destination_type, "roof_amount" => $roof_amount, "destination" => $destination, "rate_frequency" => $rate_frequency),
+              'headers' => [
+                  'Accept'     => 'application/json',
+                  'X-API-KEY'      => $this->public_key,
+                  'X-PRIVATE-API-KEY'      => $this->private_key,
+                  'X-SECRET-API-KEY'      => $this->secret,
+              ]
+          ));
+
+            $reponse = $response->getBody();
+            return json_decode((string)$reponse);
+
+        }catch (RequestException $e){
+            if ($e->hasResponse()) {
+                //$reponse = "{".$this->get_string_between(Psr7\str($e->getResponse()), "{","}")."}";
+                $reponse = Psr7\str($e->getResponse());
+                return json_decode((string)$reponse);
+            }
+            $reponse = json_encode(array( "status" => STATUS::FAILED));
+            return json_decode((string)$response);
+        }
+    }
+
+    
+    function get_string_between($string, $start, $end){
+        $string = ' ' . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return '';
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);
+    }
+
     /**
      * @return mixed
      */
